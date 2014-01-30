209dc88f43772f18270e761c344d1e37d14427f7
Kevin Counts <kevin.counts@livingsocial.com>
2014-01-30 13:13:54 -0500

    got currency conversion and find_common_currency in basic working order

--- lib/xml_parser.rb
+++ lib/xml_parser.rb
@@ -1,7 +1,7 @@
 class XMLParser
   require 'rexml/document'
 
-  def XMLParser.parse(xml)
+  def self.parse(xml)
     table = {}
     doc = REXML::Document.new(xml)
     doc.root.get_elements('rate').each do |e|
@@ -12,5 +12,19 @@ class XMLParser
       table[from][to] = conversion
     end
     table
+  end
+
+  def self.convert_through(from_a, to_a, from_b, to_b)
+    (to_a / from_a.to_f) * (to_b / from_b.to_f)
+  end
+
+  def self.convert_opposite_currency(from_currency, to_currency)
+    1.0 / (to_currency / from_currency)
+  end
+
+  def self.find_common_currency(from, to, table)
+    table[from].keys.each do |k|
+      return k if table[k].has_key?(to)
+    end
   end
 end
