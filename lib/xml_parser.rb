a1f8af8af0febed755b5b7dc8d6231aa46015b72
Kevin Counts <kevin.counts@livingsocial.com>
2014-02-08 15:51:25 -0500

    revamping

--- lib/xml_parser.rb
+++ lib/xml_parser.rb
@@ -2,13 +2,13 @@ class XMLParser
   require 'rexml/document'
   require 'set'
 
-  def self.parse(xml)
+  def self.build_table(xml)
     table = {}
     doc = REXML::Document.new(xml)
     doc.root.get_elements('rate').each do |e|
       from = e.get_text('from').to_s
       to = e.get_text('to').to_s
-      conversion = e.get_text('conversion').to_s
+      conversion = e.get_text('conversion').to_s.to_f
       table[from] ||= {}
       table[from][to] = conversion
     end
@@ -17,18 +17,19 @@ class XMLParser
 
   def self.build_permutations(table)
     s = Set.new
-    h = {}
-
     table.each do |k, v|
       s.add(k)
       v.keys.each { |k| s.add(k) }
     end
-    s.to_a.repeated_permutation(2) do |a, b|
-      next if a == b
-      h[a] ||= {}
-      h[a][b] = "0"
+    s.to_a.repeated_permutation(2) do |to, from|
+      next if to == from
+      table[to] ||={}
+
+      unless table[to][from]
+        table[to][from] = nil
+      end
     end
-    h
+    table
   end
 
   def self.convert_through(from_a, to_a, from_b, to_b)
@@ -43,5 +44,6 @@ class XMLParser
     table[from].keys.each do |k|
       return k if table[k].has_key?(to)
     end
+    return nil
   end
 end
--- spec/unit/xml_parser_spec.rb
+++ spec/unit/xml_parser_spec.rb
@@ -2,46 +2,45 @@ require 'spec_helper'
 require 'xml_parser'
 
 describe XMLParser do
+
   let(:xml_text) { IO.read(fixture('SAMPLE_RATES.xml')) }
 
-  let(:table) {
-    { "USD" => {"CAD"=>"0.9911"},
-      "CAD" => {"AUD"=>"1.0090"} }
-  }
-
-  let(:big_table) {
-    { "AUD" => {"CAD"=>"1.0079"},
-      "CAD" => {"USD"=>"1.0090"},
-      "USD" => {"CAD"=>"0.9911"} }
+  let(:table) do
+    {
+      'AUD' => { 'CAD' => 1.0079 },
+      'CAD' => { 'USD' => 1.0090 },
+      'USD' => { 'CAD' => 0.9911 },
     }
-
-  it "parses the xml text" do
-    expect(XMLParser.parse(xml_text)).to eql(
-      { "AUD" => {"CAD"=>"1.0079"},
-        "CAD" => {"USD"=>"1.0090"},
-        "USD" => {"CAD"=>"0.9911"},
-      }
-    )
   end
 
-  it "finds common currency for us -> aud" do
-    expect(XMLParser.find_common_currency('USD', 'AUD', table)).to eql('CAD')
+  let(:permutations) do
+    {
+      'AUD' => { 'CAD' => 1.0079, 'USD' => nil },
+      'CAD' => { 'USD' => 1.0090, 'AUD' => nil },
+      'USD' => { 'CAD' => 0.9911, 'AUD' => nil },
+    }
+  end
+
+  it "builds an initial table from XML" do
+    expect(XMLParser.build_table(xml_text)).to eql(table)
+  end
+
+  it "builds all permutations" do
+    expect(XMLParser.build_permutations(table)).to eql(permutations)
+  end
+
+  it "finds common currency shared by USD and AUD" do
+    expect(XMLParser.find_common_currency('USD', 'AUD', permutations)).to eql('CAD')
   end
 
   it "converts through 2 currencies" do
+    pending
     expect(XMLParser.convert_through(1, 2, 1, 2)).to eql(4.0)
   end
 
   it "reverses the rate" do
+    pending
     expect(XMLParser.convert_opposite_currency(1,2)).to eql(0.5)
   end
 
-  it "builds all permutations" do
-    expect(XMLParser.build_permutations(big_table)).to eql(
-      { "AUD" => {"CAD"=>"0", "USD"=>"0"},
-        "CAD" => {"USD"=>"0", "AUD"=>"0"},
-        "USD" => {"CAD"=>"0", "AUD"=>"0"},
-      }
-    )
-  end
 end
